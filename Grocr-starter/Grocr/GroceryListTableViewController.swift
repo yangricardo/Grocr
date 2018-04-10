7a4bdf4e04d3629effccd1f738d74a89dba6354d
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-04-10 12:49:48 -0300

    adding grocery

---
+++ Grocr-starter/Grocr/GroceryListTableViewController.swift
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) 2015 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+import Firebase
+
+class GroceryListTableViewController: UITableViewController {
+
+  // MARK: Constants
+  let listToUsers = "ListToUsers"
+  let ref = Database.database().reference(withPath: "grocery-items")
+  
+  // MARK: Properties 
+  var items: [GroceryItem] = []
+  var user: User!
+  var userCountBarButtonItem: UIBarButtonItem!
+  
+  // MARK: UIViewController Lifecycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    tableView.allowsMultipleSelectionDuringEditing = false
+    
+    userCountBarButtonItem = UIBarButtonItem(title: "1",
+                                             style: .plain,
+                                             target: self,
+                                             action: #selector(userCountButtonDidTouch))
+    userCountBarButtonItem.tintColor = UIColor.white
+    navigationItem.leftBarButtonItem = userCountBarButtonItem
+    
+    user = User(uid: "FakeId", email: "hungry@person.food")
+  }
+  
+  // MARK: UITableView Delegate methods
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return items.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+    let groceryItem = items[indexPath.row]
+    
+    cell.textLabel?.text = groceryItem.name
+    cell.detailTextLabel?.text = groceryItem.addedByUser
+    
+    toggleCellCheckbox(cell, isCompleted: groceryItem.completed)
+    
+    return cell
+  }
+  
+  override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    return true
+  }
+  
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+      items.remove(at: indexPath.row)
+      tableView.reloadData()
+    }
+  }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    guard let cell = tableView.cellForRow(at: indexPath) else { return }
+    var groceryItem = items[indexPath.row]
+    let toggledCompletion = !groceryItem.completed
+    
+    toggleCellCheckbox(cell, isCompleted: toggledCompletion)
+    groceryItem.completed = toggledCompletion
+    tableView.reloadData()
+  }
+  
+  func toggleCellCheckbox(_ cell: UITableViewCell, isCompleted: Bool) {
+    if !isCompleted {
+      cell.accessoryType = .none
+      cell.textLabel?.textColor = UIColor.black
+      cell.detailTextLabel?.textColor = UIColor.black
+    } else {
+      cell.accessoryType = .checkmark
+      cell.textLabel?.textColor = UIColor.gray
+      cell.detailTextLabel?.textColor = UIColor.gray
+    }
+  }
+  
+  // MARK: Add Item
+  
+  @IBAction func addButtonDidTouch(_ sender: AnyObject) {
+    let alert = UIAlertController(title: "Grocery Item",
+                                  message: "Add an Item",
+                                  preferredStyle: .alert)
+    
+    let saveAction = UIAlertAction(title: "Save",
+                                   style: .default) {  _ in
+      // 1 Get the text field (and its text) from the alert controller.
+      guard let textField = alert.textFields?.first,
+        let text = textField.text else { return }
+      
+      // 2 Using the current user’s data, create a new GroceryItem that is not completed by default.
+      let groceryItem = GroceryItem(name: text,
+                                    addedByUser: self.user.email,
+                                    completed: false)
+      // 3 Create a child reference using child(_:). The key value of this reference is the item’s name in lowercase, so when users add duplicate items (even if they capitalize it, or use mixed case) the database saves only the latest entry.
+      let groceryItemRef = self.ref.child(text.lowercased())
+      
+      // 4 Use setValue(_:) to save data to the database. This method expects a Dictionary. GroceryItem has a helper function called toAnyObject() to turn it into a Dictionary.
+      groceryItemRef.setValue(groceryItem.toAnyObject())
+    }
+    
+    let cancelAction = UIAlertAction(title: "Cancel",
+                                     style: .default)
+    
+    alert.addTextField()
+    
+    alert.addAction(saveAction)
+    alert.addAction(cancelAction)
+    
+    present(alert, animated: true, completion: nil)
+  }
+  
+  func userCountButtonDidTouch() {
+    performSegue(withIdentifier: listToUsers, sender: nil)
+  }
+  
+}
